f762ea8b4c6a532d37b2884c4bd29b3364559087
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-11-29 17:57:15 +0530

    cart component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from "react";
-import Cart from "./components/Cart/Cart";
+// import React from "react";
+// import Cart from "./components/Cart/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Cart</h1>
-      <Cart />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Cart</h1>
+//       <Cart />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0
+    };
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.value > 5) return false;
+    return true;
+  }
+  componentDidUpdate() {
+    console.log("->");
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((prevState) => {
+        return {
+          value: prevState.value + 1
+        };
+      });
+    }, 1000);
+  }
+  render() {
+    console.log(this.state.value);
+    return <div>{this.state.value}</div>;
+  }
 }
 
-export default App;
+export default App
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import './Cart.css';
 
 function Cart() {
+
+
   return (
-    <>
+
+<>
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div>Phone</div>
-          <div>Rs 999</div>
-          <div>Qty: 1</div>
-          <div className="cart-item-actions">{/* {Buttons} */}</div>
+          <div style={{fontSize: 25}}>Phone</div>
+          <div style={{color:'grey'}}>Rs 999</div>
+          <div style={{color:'grey'}}>Qty: 1</div>
+          <div className="cart-item-actions">
+            {/* {Buttons} */}
+            <img src="https://cdn-icons-png.flaticon.com/512/992/992651.png" className="action-icons" alt="increase" />
+            <img src="https://cdn-icons-png.flaticon.com/512/992/992683.png" className="action-icons" alt="deacrease" />
+            <img src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" className="action-icons" alt="delete" />
+            
+            </div>
         </div>
       </div>
     </>
